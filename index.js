15970f5cc2728ae2493f3ee403edc903d2c11370
Andrey <shmalunandrey@gmail.com>
2022-08-21 19:03:07 -0400

    fixed CORS errors

--- index.js
+++ index.js
@@ -21,9 +21,22 @@ app.use(express.static('public'));
 app.use(morgan('common'));
 
 require('./auth')(app);
+
 const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 const { check, validationResult } = require('express-validator');
-app.use(cors());
 const passport = require('passport');
 require('./passport');
 
