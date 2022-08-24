0c9048ee9a82b3d9231540d791a2784d62008f5e
Andrey <shmalunandrey@gmail.com>
2022-08-23 23:33:55 -0400

    Update CORS policy

--- index.js
+++ index.js
@@ -22,19 +22,27 @@ app.use(morgan('common'));
 
 require('./auth')(app);
 
-const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'https://movio-app.herokuapp.com'];
+// const cors = require('cors');
+// let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'https://movio-app.herokuapp.com'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+
+const cors = require('cors');
+const corsOptions = {
+  origin: ['http://localhost:1234', 'http://localhost:8080', 'https://movio-app.herokuapp.com'],
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 const { check, validationResult } = require('express-validator');
 const passport = require('passport');
