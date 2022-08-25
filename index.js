81276889b0544dbfe709ec7ac92b8c4c88024691
Andrey <shmalunandrey@gmail.com>
2022-08-24 20:39:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,18 +23,20 @@ app.use(morgan('common'));
 require('./auth')(app);
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'https://movio-app.herokuapp.com'];
+app.use(cors());
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// let allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'https://movio-app.herokuapp.com'];
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 // const cors = require('cors');
 // const corsOptions = {
